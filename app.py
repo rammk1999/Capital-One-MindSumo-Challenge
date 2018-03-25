5315ada96a6a3f4a67c6233f48745179d5c1e4ba
rmuthu4 <rmuthu4@users.noreply.github.com>
2018-03-25 07:08:09 -0500

    Added some touches to the web site to describe the prediction method.

--- app.py
+++ app.py
@@ -15,10 +15,18 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def predict_dispatch():
     # add more code here to predict the unit type that is dispatched
-    return render_template('index.html')
+    if request.method == "POST":
+        input_lat = request.form.get("latitude")
+        input_long = request.form.get("longitude")
+        user_time = request.form.get("time")
+        classifier = predictor.DispatchUnitPredictor()
+        most_likely_dispatch = classifier.predict_dispatch(user_latitude=float(input_lat),
+                                                           user_longitude=float(input_long), user_time=user_time)
+        print(most_likely_dispatch)
+        return render_template('index.html', response=most_likely_dispatch)
 
 
 if __name__ == "__main__":
--- predictor.py
+++ predictor.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 from sklearn.neighbors import KNeighborsRegressor
 import time
+import  datetime
 
 # class InputData:
 #     def __init__(self, latitude, longitude, timestamp):
@@ -34,19 +35,27 @@ class DispatchUnitPredictor:
         return y_cols
 
 
-    def _predict_dispatch(self, user_latitude, user_longitude, user_time ):
+    def predict_dispatch(self, user_latitude, user_longitude, user_time):
         knn = KNeighborsRegressor(n_neighbors=self.neighbors, weights="distance", algorithm="auto")
         train_x = [self.x_columns]
+        print(train_x)
         train_y = [self.y_columns]
+        print(train_y)
+        print("About to fit the model")
         knn.fit(train_x, train_y)
-        prediction = "unit_type"
+        print("Fit the model")
         input_time = time.strptime(user_time, "%H:%M:%S")
-        prediction = knn.predict([float(user_latitude), float(user_longitude), input_time])
+        input_time = datetime.timedelta(hours=input_time.tm_hour, minutes=input_time.tm_min,
+                                        seconds=input_time.tm_sec).total_seconds()
+        print(input_time)
+        print("Converted the data")
+        test_x = [[input_time, user_latitude, user_longitude]]
+        print(test_x)
+        prediction = knn.predict(test_x)
+        print("Predicted the answer")
         return prediction
-
-
-
 
 
 if __name__ == '__main__':
     test = DispatchUnitPredictor()
+    test.predict_dispatch(37.7744, -122.5046, "17:36:16")
