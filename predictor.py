7a99e207b52ee4bde86e70f1d792f7a55358a278
rmuthu4 <rmuthu4@users.noreply.github.com>
2018-03-25 04:57:29 -0500

    Finished writing the first version of a KNN classifier.

--- predictor.py
+++ predictor.py
@@ -1,25 +1,52 @@
 import pandas as pd
 import numpy as np
 import math
+from sklearn.neighbors import KNeighborsRegressor
+import time
 
-from sklearn.utils import shuffle
-from datetime import datetime
-from collections import defaultdict,Counter
-
-import csv
-import random
-
-
-
-class InputData:
-    def __init__(self, latitude, longitude, timestamp):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.time_in_seconds = timestamp
+# class InputData:
+#     def __init__(self, latitude, longitude, timestamp):
+#         self.latitude = latitude
+#         self.longitude = longitude
+#         self.time_in_seconds = time.strptime(timestamp, "%H:%M:%S")
 
 
 # finish the prediction algorithm here
 class DispatchUnitPredictor:
-    def __init__(self):
-        self.self = self
+    def __init__(self, data="./data/sfpd_filtered_for_predictions.csv", neighbors=1, verified=False):
+        self.data_frame = pd.read_csv(data)
+        self.neighbors = neighbors
+        self.x_columns = self._get_x_cols(self.data_frame)
+        self.y_columns = self._get_y_cols(self.data_frame)
 
+
+    @staticmethod
+    def _get_x_cols(df):
+        all_cols = list(df)
+        x_cols = all_cols[:-1]
+        return x_cols
+
+
+    @staticmethod
+    def _get_y_cols(df):
+        all_cols = list(df)
+        y_cols = all_cols[-1:]
+        return y_cols
+
+
+    def _predict_dispatch(self, user_latitude, user_longitude, user_time ):
+        knn = KNeighborsRegressor(n_neighbors=self.neighbors, weights="distance", algorithm="auto")
+        train_x = [self.x_columns]
+        train_y = [self.y_columns]
+        knn.fit(train_x, train_y)
+        prediction = "unit_type"
+        input_time = time.strptime(user_time, "%H:%M:%S")
+        prediction = knn.predict([float(user_latitude), float(user_longitude), input_time])
+        return prediction
+
+
+
+
+
+if __name__ == '__main__':
+    test = DispatchUnitPredictor()
