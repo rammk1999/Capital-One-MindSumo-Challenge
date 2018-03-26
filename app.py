3edcb08573f376625c721d7d53b004c756d699d6
rmuthu4 <rmuthu4@users.noreply.github.com>
2018-03-25 19:21:53 -0500

    Added exception handling to the website. Modified the data to make the KNN model better.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap
 import csv
 import predictor
-import datetime
+import time
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,19 +15,32 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/dispatch_predictor", methods=["GET", "POST"])
 def predict_dispatch():
-    # add more code here to predict the unit type that is dispatched
     if request.method == "POST":
+        global response
         input_lat = request.form.get("latitude")
         input_long = request.form.get("longitude")
         user_time = request.form.get("time")
-        classifier = predictor.DispatchUnitPredictor()
-        most_likely_dispatch = classifier.predict_dispatch(user_latitude=float(input_lat),
-                                                           user_longitude=float(input_long), user_time=user_time)
-        print(most_likely_dispatch)
-        return render_template('index.html', response=most_likely_dispatch)
+        try:
+            user_time = time.strptime(user_time, "%H:%M:%S")
+            classifier = predictor.DispatchUnitPredictor()
+            dispatch_code = classifier.predict_dispatch(user_latitude=float(input_lat),
+                                                        user_longitude=float(input_long), user_time=user_time)
+            units = ["MEDIC", "ENGINE", "PRIVATE", "TRUCK", "CHIEF",
+                     "RESCUE CAPTAIN", "RESCUE SQUAD", "SUPPORT", "INVESTIGATION"]
+            most_likely_dispatch = ""
+            for dispatch in units:
+                if dispatch == int(round(dispatch_code)):
+                    most_likely_dispatch = dispatch
+
+            response="Most likely dispatch :" + most_likely_dispatch
+
+        except ValueError:
+            response = "Time was inputted incorrectly, re-enter your input"
+
+    return render_template('index.html', response=response)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
--- predictor.py
+++ predictor.py
@@ -8,7 +8,7 @@ import  datetime
 
 # finish the prediction algorithm here
 class DispatchUnitPredictor:
-    def __init__(self, data="./data/sfpd_filtered_for_predictions.csv", neighbors=1):
+    def __init__(self, data="./data/sfpd_filtered_for_predictions.csv", neighbors=9):
         self.data_frame = pd.read_csv(data)
         self.neighbors = neighbors
         self.x_columns = self._get_x_cols(self.data_frame)
@@ -19,7 +19,6 @@ class DispatchUnitPredictor:
     def _get_x_cols(df):
         all_cols = list(df)
         x_cols = all_cols[:-2]
-        print(x_cols)
         return x_cols
 
 
@@ -27,23 +26,21 @@ class DispatchUnitPredictor:
     def _get_y_cols(df):
         all_cols = list(df)
         y_cols = all_cols[-1:]
-        print(y_cols)
         return y_cols
 
 
     def predict_dispatch(self, user_latitude, user_longitude, user_time):
         train_x = pd.read_csv(filepath_or_buffer="./data/sfpd_filtered_for_predictions.csv", usecols=self.x_columns)
         train_y = pd.read_csv(filepath_or_buffer="./data/sfpd_filtered_for_predictions.csv", usecols=self.y_columns)
-        knn = KNeighborsRegressor(n_neighbors=self.neighbors)
+        knn = KNeighborsRegressor(n_neighbors=self.neighbors, weights="distance", algorithm="auto")
         knn.fit(train_x, train_y)
         input_time = time.strptime(user_time, "%H:%M:%S")
-        input_time = datetime.timedelta(hours=input_time.tm_hour, minutes=input_time.tm_min,
-                                        seconds=input_time.tm_sec).total_seconds()
+        # took the mod of each unit of time by the standard amount of time so that the time entered by the user
+        # is somewhat accurate and within a 24 time frame
+        input_time = datetime.timedelta(hours=input_time.tm_hour % 24, minutes=input_time.tm_min % 60,
+                                        seconds=input_time.tm_sec % 60).total_seconds()
         test_x = [[float(input_time), float(user_latitude), float(user_longitude)]]
         prediction = knn.predict(test_x)
-        return prediction
+        print(prediction[0][0])
+        return prediction[0][0]
 
-
-if __name__ == '__main__':
-    test = DispatchUnitPredictor()
-    test.predict_dispatch(37.7552, -122.4755, "2:04:21")
